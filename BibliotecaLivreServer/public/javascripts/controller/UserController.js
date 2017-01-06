07133ce093070a1611234770224884a35db9afd3
Neto Deolino <netodeolino@gmail.com>
2017-01-06 10:34:50 -0300

    06-01-2017

--- BibliotecaLivreServer/public/javascripts/controller/UserController.js
+++ BibliotecaLivreServer/public/javascripts/controller/UserController.js
@@ -17,3 +17,72 @@ exports.saveUser = function (req, res, next) {
 		res.redirect('/');
 	});
 }
+
+/* TO EDIT OF THE MY PROJECT
+// query db for all todo items
+exports.index = function ( req, res ){
+  Todo.find( function ( err, todos, count ){
+    res.render( 'index', {
+      title : 'Express Todo Example',
+      todos : todos
+    });
+  });
+};
+
+// remove todo item by its id
+exports.destroy = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.remove( function ( err, todo ){
+      res.redirect( '/' );
+    });
+  });
+};
+
+exports.edit = function ( req, res ){
+  Todo.find( function ( err, todos ){
+    res.render( 'edit', {
+        title   : 'Express Todo Example',
+        todos   : todos,
+        current : req.params.id
+    });
+  });
+};
+
+
+// redirect to index when finish
+exports.update = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save( function ( err, todo, count ){
+      res.redirect( '/' );
+    });
+  });
+};
+
+exports.index = function ( req, res ){
+  Todo.
+    find().
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      res.render( 'index', {
+          title : 'Express Todo Example',
+          todos : todos
+      });
+    });
+};
+ 
+exports.edit = function ( req, res ){
+  Todo.
+    find().
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      res.render( 'edit', {
+          title   : 'Express Todo Example',
+          todos   : todos,
+          current : req.params.id
+      });
+    });
+};
+
+*/
