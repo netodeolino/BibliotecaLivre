2348786f1d542d058fad43d9110a803f77f53ef7
Neto Deolino <netodeolino@gmail.com>
2017-01-29 21:44:49 -0300

    29-01-2017

--- BibliotecaLivreApp/app/src/main/java/com/deolino/neto/bibliotecalivre/activies/LoginActivity.java
+++ BibliotecaLivreApp/app/src/main/java/com/deolino/neto/bibliotecalivre/activies/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.deolino.neto.bibliotecalivre.activies;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.deolino.neto.bibliotecalivre.R;
 import com.deolino.neto.bibliotecalivre.interfaces.ServerResponseListener;
 import com.deolino.neto.bibliotecalivre.server.Response;
 
@@ -11,7 +13,13 @@ import com.deolino.neto.bibliotecalivre.server.Response;
 
 public class LoginActivity extends AppCompatActivity implements ServerResponseListener {
 
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        
+    }
 
     @Override
     public void onSuccess(Response response, String requestUrl) {
