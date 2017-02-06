6b53d6e4ecfb84c93a4ca55fd096c81dca58d03d
Neto Deolino <netodeolino@gmail.com>
2017-02-06 13:55:29 -0300

    06-02-2017

--- BibliotecaLivreApp/app/src/main/java/com/deolino/neto/bibliotecalivre/activies/ResetPasswordActivity.java
+++ BibliotecaLivreApp/app/src/main/java/com/deolino/neto/bibliotecalivre/activies/ResetPasswordActivity.java
@@ -1,15 +1,35 @@
 package com.deolino.neto.bibliotecalivre.activies;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.deolino.neto.bibliotecalivre.R;
 import com.deolino.neto.bibliotecalivre.interfaces.ServerResponseListener;
 import com.deolino.neto.bibliotecalivre.server.Response;
+import com.deolino.neto.bibliotecalivre.server.ServerRequest;
 
 /**
  * Created by neto on 30/01/17.
  */
 
 public class ResetPasswordActivity extends AppCompatActivity implements ServerResponseListener {
+
+    private EditText editTextMail;
+    private ServerRequest request;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        this.request = new ServerRequest(this, this);
+
+        this.editTextMail = (EditText) findViewById(R.id.etResetPassword);
+    }
+
     @Override
     public void onSuccess(Response response, String requestUrl) {
 
@@ -18,5 +38,15 @@ public class ResetPasswordActivity extends AppCompatActivity implements ServerRe
     @Override
     public void onFailure(Response response, String requestUrl) {
 
+    }
+
+    public void resetPassword(View view) {
+        String email = editTextMail.getText().toString();
+
+        if (email.isEmpty()) {
+            Toast.makeText(ResetPasswordActivity.this, getString(R.string.error_empty_fields), Toast.LENGTH_LONG).show();
+        } else {
+            
+        }
     }
 }
