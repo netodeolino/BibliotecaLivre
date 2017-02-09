aeeb99eeadddec9f382b516f8ce56b0dc0fee85c
Neto Deolino <netodeolino@gmail.com>
2017-02-09 11:03:27 -0300

    09-02-2017

--- BibliotecaLivreServer/app.js
+++ BibliotecaLivreServer/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 // open connection
 require('./public/javascripts/banco/ConexaoBanco');
@@ -18,12 +19,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// session
+app.use(session({
+  secret: 'qwe',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(function(req, res, next){
+  res.locals.session = req.session.emailUser;
+  next();
+});
 
 app.use('/', index);
 app.use('/web', web)
